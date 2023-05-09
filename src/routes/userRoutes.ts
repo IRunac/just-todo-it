13fc380c8f2946065d705240f3420753a300be4a
Ivo Runac <ivo.runac@gmail.com>
2023-05-09 16:58:00 +0200

    Modify Request to accomodate User in user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,16 +1,21 @@
 import express, { Request, Response } from 'express';
 import { DependecyInjection } from '../index';
+import { User } from '../entities/User';
 
 export const userRoutesInit = (DI: DependecyInjection) => {
   const userRepository = DI.userRepository;
   const entityManager = DI.em;
   const router = express.Router();
 
-  router.param('id', async (req: Request, res: Response, next, id) => {
+  interface UserRequest extends Request {
+    user?: User
+  }
+
+  router.param('id', async (req: UserRequest, res: Response, next, id) => {
     const userId: number = parseInt(id);
     const user = await userRepository.findOne({ id: userId });
     if (!user) return res.status(404).send('User not found');
-    req.body.user = user;
+    req.user = user;
     next();
   });
 
@@ -21,8 +26,8 @@ export const userRoutesInit = (DI: DependecyInjection) => {
   });
 
   // GET BY ID
-  router.get('/:id', async (req: Request, res: Response) => {
-    return res.status(200).send(req.body.user);
+  router.get('/:id', async (req: UserRequest, res: Response) => {
+    return res.status(200).send(req.user);
   });
 
   // POST
@@ -33,14 +38,15 @@ export const userRoutesInit = (DI: DependecyInjection) => {
   });
 
   // DELETE
-  router.delete('/:id', async (req: Request, res: Response) => {
-    await entityManager.removeAndFlush(req.body.user);
+  router.delete('/:id', async (req: UserRequest, res: Response) => {
+    await entityManager.removeAndFlush(req.user!);
     return res.sendStatus(204);
   });
 
   // PATCH
-  router.patch('/:id', async (req: Request, res: Response) => {
-    const { user, username, role } = req.body;
+  router.patch('/:id', async (req: UserRequest, res: Response) => {
+    const { username, role } = req.body;
+    const user = req.user!;
     user.username = username || user.username;
     user.role = role || user.role;
     await entityManager.persistAndFlush(user);
